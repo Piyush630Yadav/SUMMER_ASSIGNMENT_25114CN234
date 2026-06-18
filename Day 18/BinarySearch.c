be7a7dd1d75208ae667e06eb86bc2dbf87f97dda
Piyush Yadav <6306084640py@gmail.com>
2026-06-18 21:36:29 +0530

    Add files via upload

---
+++ Day 18/BinarySearch.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n, i, key, low, high, mid;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+    int a[n];
+
+    printf("Enter elements: ");
+    for(i= 0; i < n; i++)
+    scanf("%d",&a[i]);
+
+    printf("Enter key: ");
+    scanf("%d",&key);
+
+    low= 0;
+    high= n - 1;
+
+    while(low <= high){
+        mid = (low + high)/ 2;
+        if(a[mid] == key){
+            printf("Element found at position %d", mid + 1);
+            return 0;
+        }
+        else if(a[mid] < key)
+        low= mid + 1;
+        else 
+        high= mid -1;
+    }
+    printf("Element not found");
+
+    return 0;
+}
